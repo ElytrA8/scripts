3dc2e59b1b13c38b0b83e065d17278df4d87a64f
Shéikh Adnan <manofuranium@gmail.com>
2020-12-21 16:31:34 +0530

    Update shrp.sh

--- shrp.sh
+++ shrp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/ElytrA8/recovery_rosy.git device/xiaomi/rosy
+git clone https://github.com/SHRP-Devices/recovery-device-xiaomi-rosy.git device/xiaomi/rosy
 git clone https://github.com/omnirom/android_vendor_qcom_opensource_commonsys -b android-9.0 vendor/qcom/opensource/commonsys
 . build/envsetup.sh
 lunch omni_rosy-eng
